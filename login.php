03c8206d7498f9e2db8591470f47daa2e3372078
cristianmarinprogrammer <166737408+cristianmarinprogrammer@users.noreply.github.com>
2024-10-18 12:34:01 -0500

    Add files via upload
    MODIFICACION LOGINS

--- login.php
+++ login.php
@@ -31,61 +31,36 @@ if($_POST){
 }
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Login</title>
-        <!-- Required meta tags -->
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-
-        <!-- Bootstrap CSS v5.2.1 -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"  integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"crossorigin="anonymous"/>
-    </head>
-
-    <body>
-      
-      <div class="container">
-
-              <div class="row">
-
-                <div class="col-md-4">
-                    
-                </div>
-
-                <div class="col-md-4">
-                 <br/>
-                 <div class="card">
-                 <div class="card-header">Iniciar sesión</div>
-                      <div class="card-body">
-                          <form action="login.php" method="post" autocomplete="off">
-
-                              Usuario: <input class="form-control" type="text" name="usuario" id="" required>
-                              </br>
-                              Contraseña: <input class="form-control" type="password" name="contrasenia" id="" required>
-                              </br>
-                              <a  href = "recuperar_contrasenia.php"> ¿Olvidó su contraseña?</a>
-                              </br>
-                              </br>
-                              <button class="btn btn-success" type="submit">Entrar a la galería</button>
-                              </br> 
-                              <br>
-                              <a  href = "registro.php"> ¿No tienes cuenta?</a>
-                              <br>
-                          </form>
-                      </div>
-                 <div class="card-footer text-muted"></div>
-                </div>
-               
-                </div>
-
-                <div class="col-md-4">
-                    
-                </div>
-                 
-              </div>
-
-        </div>
-
-    </body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="Login.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+  <div class="wrapper">
+    <form action="login.php" method="POST">
+      <h1>Inicar Sesión </h1>
+      <div class="input-box">
+        <input type="text" placeholder="Usuario" name="usuario" required>
+       <i class='bx bxs-user'></i>
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Contraseña" name="contrasenia" required>
+        <i class='bx bxs-lock-alt' ></i>
+      </div>
+      <div class="remember-forgot">
+        <label><input type="checkbox">Recuerdame</label>
+        <a href="recuperar_contrasenia.php">¿Olvidaste tu contraseña?</a>
+      </div>
+      <button type="submit" class="btn">Iniciar sesión</button>
+      <div class="register-link">
+        <p>¿No tienes cuenta? <a href="registro.php">Registrate</a></p>
+      </div>
+    </form>
+  </div>
+</body>
 </html>
--- registro.php
+++ registro.php
@@ -42,45 +42,33 @@ if ($_POST) {
 }
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Registro</title>
-        <!-- Required meta tags -->
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-
-        <!-- Bootstrap CSS v5.2.1 -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"  integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"crossorigin="anonymous"/>
-    </head>
-
-    <body>
-      
-      <div class="container">
-              <div class="row">
-                <div class="col-md-4"></div>
-                <div class="col-md-4">
-                 <br/>
-                 <div class="card">
-                 <div class="card-header">Registro de usuario</div>
-                      <div class="card-body">
-                          <form action="registro.php" method="post">
-                              Usuario: <input class="form-control" type="text" name="usuario" id="" required>
-                              <br/>
-                              Correo: <input class="form-control" type="email" name="correo" id="" required 
-                              title="Debe ingresar un correo válido.">
-                              <br/>
-                              Contraseña: <input class="form-control" type="password" name="contrasenia" id="" required
-                              pattern="^\S{8,}$" title="La contraseña debe tener al menos 8 caracteres y no contener espacios.">
-                              <br/>
-                              <button class="btn btn-primary" type="submit">Registrar</button>
-                          </form>
-                      </div>
-                 <div class="card-footer text-muted"></div>
-                </div>
-                </div>
-                <div class="col-md-4"></div>
-              </div>
-        </div>
-    </body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registro</title>
+  <link rel="stylesheet" href="Registro.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+  <div class="wrapper">
+    <form action="login.php" method="POST">
+      <h1>Registro </h1>
+      <div class="input-box">
+        <input type="text" placeholder="Usuario" name="usuario" required>
+        <i class='bx bxs-user'></i>
+      </div>
+      <div class="input-box">
+        <input type="text" placeholder="Correo" name="correo" required>
+        <i class='bx bxl-gmail'></i>  
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Contraseña" name="contrasenia" required>
+        <i class='bx bxs-lock-alt' ></i>
+      </div>
+      <button type="submit" class="btn">Registrame</button>
+    </form>
+  </div>
+</body>
 </html>
